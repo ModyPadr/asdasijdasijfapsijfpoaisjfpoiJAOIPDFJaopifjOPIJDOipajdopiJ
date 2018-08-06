7c82beb11d41e9d107de2a6b782ef7488fba0965
ModyPadr <36020255+ModyPadr@users.noreply.github.com>
2018-08-06 07:05:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,8 +79,36 @@ if (message.content.startsWith(admin + 'setavatar')) {
 }
 });  
 
+client.on('message', message => {
+if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`⚠️ Restarting : ${message.author.username}`);
+        console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(`تم اعادة تشغيل البوت`);
+    }
+  
+  });
+
 
 //======================================[ Log ]======================================
+
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if (message.content.startsWith(prefix + 'ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setColor('#36393e')
+.addField(' Time Taken : ',msg + " ms")
+.addField(' WebSocket : ',api + " ms")
+message.channel.send({embed:embed});
+}
+});
 
   client.on('messageUpdate', (message, newMessage) => {
     if (message.content === newMessage.content) return;
@@ -282,6 +310,48 @@ client.on('roleCreate', role => {
   
 //======================================[Commands]======================================
   
+
+client.on('message', message => {
+  if(!message.channel.guild) return;
+     if(message.author.id !== '419212015098134538') return;
+if(message.content.startsWith(prefix +'bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let copy = "Premuim";
+let request = `Requested By ${message.author.username}`;
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('Broadcast')
+.addField('Server', message.guild.name)
+.addField('Sender', message.author.username)
+.addField('Message', args)
+.setThumbnail(message.author.avatarURL)
+.setFooter(copy, client.user.avatarURL);
+m.send({ embed: bc })
+msg.delete();
+})
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
   
   client.on('message', msg => {
   if (msg.author.bot) return;
