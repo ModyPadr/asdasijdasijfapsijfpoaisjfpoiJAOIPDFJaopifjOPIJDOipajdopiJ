be0e1190332191ea00209c422274f34df4ceaf76
ModyPadr <36020255+ModyPadr@users.noreply.github.com>
2018-08-06 14:55:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -941,5 +941,82 @@ client.on('message' , async (message) => {
 }); 
 
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`** :hearts: ولكم نورت السيرفر :hearts:
+  :grinning:  أسم العضو --> __${member}__ :grinning: 
+  :bust_in_silhouette:  انت عضو رقم --> __${member.guild.memberCount}__ :bust_in_silhouette:
+  :airplane: __${moment().format('HH:mm:ss A')}__ <-- تاريخ دخولك السيرفر ** :airplane: `)
+}).catch(console.error)
+})
+
+
+
+      bot.on('guildMemberAdd', member => {
+      const welcomer =  member.guild.channels.find('name', 'welcome-goodbye'); //اسم الروم
+
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+
+      const w = ['./imgs/w1.png',
+      './imgs/w1.png',];
+
+              let Image = Canvas.Image,
+                  canvas = new Canvas(401, 202),
+                  ctx = canvas.getContext('2d');
+              ctx.patternQuality = 'bilinear';
+              ctx.filter = 'bilinear';
+              ctx.antialias = 'subpixel';
+              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+              ctx.shadowOffsetY = 2;
+              ctx.shadowBlur = 2;
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 401, 202);
+
+      })
+
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+
+                              //AVATAR�
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.drawImage(ava, 152, 27, 95, 95);
+
+                                                      //wl
+                              ctx.font = '20px Arial Bold';
+                              ctx.fontSize = '15px';
+                              ctx.fillStyle = "#f20000";
+                              ctx.textAlign = "center";
+                                                         ctx.fillText(member.user.username, 200, 154);
+
+                              //NAME�
+                              ctx.font = '20px Arial';
+                              ctx.fontSize = '28px';
+                              ctx.fillStyle = "#f20000";
+                              ctx.textAlign = "center";
+                                    ctx.fillText(`انت العضو رقم ${member.guild.memberCount} `
+                              , 200, 190);
+
+ welcomer.sendFile(canvas.toBuffer())
+
+
+
+      })
+      })
+      });
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
