ae3096eadbd213fd15916ed2e40147aa7c1c1629
Rainbow <36020255+ModyPadr@users.noreply.github.com>
2018-10-21 04:10:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,25 +80,6 @@ if (message.content.startsWith(admin + 'setavatar')) {
 
 
 //======================================[ Log ]======================================
-      client.on('message', message => {
-        var argresult = message.content.split(` `).slice(1).join(' ');
-          if (!devs.includes(message.author.id)) return;
-          
-        if (message.content === (adminprefix + "Percie")) {
-        message.guild.leave();        
-      } else     
-        if(message.content === adminprefix + "restart") {
-          if(!devs.includes(message.author.id)) return;
-              message.channel.send(`⚠️ **Bot restarting by ${message.author.username}**`);
-            console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            console.log(`⚠️ Bot restarting... ⚠️`);
-            console.log("===============================================\n\n");
-            client.destroy();
-            child_process.fork(__dirname + "/bot.js");
-            console.log(`Bot Successfully Restarted`);
-        }
-      
-      });
 
 client.on('message', message => {
     if(!message.channel.guild) return;
